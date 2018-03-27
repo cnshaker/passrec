ed059cc5e2b67c4edd4f430415cb40d6ba3be4d7
shaker <shaker@163.com>
2018-03-27 09:00:04 +0800

    *

--- src/passrec.c
+++ src/passrec.c
@@ -5,27 +5,36 @@
 //呵呵
 int main(int argc,char* argv[])
 {
-	int i;
-	for(i=0;i<argc;i++)
+	int i,l;
+	if(argc<=1)
 	{
-		printf("%d ==> %s\n",i,argv[i]);
+		printf("Usage:\n\t%s filename\n",argv[0]);
+		return 0;
 	}
-	FILE *pf=fopen("e:\\20180312-0318_passrec.csv","r");
+	FILE *pf=fopen(argv[1],"r");
 	if(pf==NULL)
 	{
-		printf("Open file fault!\n");
+		printf("Error:\n\tOpen file fault!\n");
 		return -1;
 	}
 	char buffer[1024];
+	char szBuf[128];
 	for(i=0;i<2;i++)
 	{
 		fgets(buffer,1024,pf);
+		//printf("%s\n",buffer);
+		l=strlen(buffer)-1;
+		while((buffer[l]==(char)0xa)||(buffer[l]==(char)0xd)){buffer[l]='\0';l--;}
 		const char * d=",";
 		char *p;
 		p=strtok(buffer,d);
 		do
 		{
-			printf("%s\n",p);
+			while((*p=='"')||(*p==' '))p++;
+			strcpy(szBuf,p);
+			l=strlen(szBuf)-1;
+			while((szBuf[l]=='"')||(szBuf[l]==' ')){szBuf[l]='\0';l--;}
+			printf("%s\n",szBuf);
 		}while((p=strtok(NULL,d))!=NULL);
 	}
 	fclose(pf);
